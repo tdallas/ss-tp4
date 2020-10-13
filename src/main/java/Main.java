c7959e1197b91ba1d65fe56453b0a00ef482f4b0
tdorado <tdorado@itba.edu.ar>
2020-10-12 22:43:08 -0300

    clean main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,50 +1,19 @@
 import org.apache.commons.cli.*;
+import systems.oscillator.OscillatorSystem;
 import systems.planets.PlanetSystem;
 
-
 public class Main {
-    private static final double SPACESHIP_MASS = 5 * Math.pow(10, 5);                    // kg
-    private static final double SPACESHIP_RADIUS = 50;                                   // m
-    private static final double SPACESHIP_HEIGHT = 1500 * Math.pow(10, 3);               // m
-    private static final double SPACESHIP_ORBITAL_VELOCITY = 7.12 * Math.pow(10, 3);     // m/s
-    private static final double SPACESHIP_LAUNCH_VELOCITY_0 = 8 * Math.pow(10, 3);       // m/s
-    private static final double SPACESHIP_LAUNCH_VELOCITY_1 = 8 * Math.pow(10, 3);     // m/s
-
     private static int system;
-    private static String filename;
-    private static double deltaTime;
-    private static double saveDeltaTime;
 
     public static void main(String[] args) {
         //parseArguments(args);
 
-        //Print input
-//        System.out.println("--------------------------");
-//        System.out.println("Input: ");
-//        System.out.println("Time delta: " + deltaTime);
-//        System.out.println("Save time delta: " + saveDeltaTime);
-//        System.out.println("File name: " + deltaTime);
-//        System.out.println("--------------------------");
-
-//        OscillatorSystem.runOscillators();
-
+        OscillatorSystem.runOscillators();
         PlanetSystem.runPlanets();
     }
 
     private static void parseArguments(String[] args) {
         Options options = new Options();
-
-        Option outputOption = new Option("o", "output", true, "output file name");
-        outputOption.setRequired(true);
-        options.addOption(outputOption);
-
-        Option deltaTimeOption = new Option("dt", "time-delta", true, "time delta for animations");
-        deltaTimeOption.setRequired(true);
-        options.addOption(deltaTimeOption);
-
-        Option saveDeltaTimeOption = new Option("sdt", "save-time-delta", true, "time delta for saving");
-        saveDeltaTimeOption.setRequired(true);
-        options.addOption(saveDeltaTimeOption);
 
         Option systemOption = new Option("s", "system", true, "system to simulate");
         systemOption.setRequired(true);
@@ -64,28 +33,6 @@ public class Main {
         }
 
         try {
-            deltaTime = Double.parseDouble(cmd.getOptionValue("time-delta"));
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid argument number of particles, must be double");
-            System.exit(1);
-        }
-        if (deltaTime < 0) {
-            System.out.println("Invalid time delta, must be positive");
-            System.exit(1);
-        }
-
-        try {
-            saveDeltaTime = Double.parseDouble(cmd.getOptionValue("save-time-delta"));
-        } catch (NumberFormatException e) {
-            System.out.println("Invalid argument number of particles, must be double");
-            System.exit(1);
-        }
-        if (saveDeltaTime < 0) {
-            System.out.println("Invalid save time delta, must be positive");
-            System.exit(1);
-        }
-
-        try {
             system = Integer.parseInt(cmd.getOptionValue("system"));
         } catch (NumberFormatException e) {
             System.out.println("Invalid argument system, must be integer between 1 and 5");
@@ -95,7 +42,5 @@ public class Main {
             System.out.println("Invalid argument system, must be integer between 1 and 5");
             System.exit(1);
         }
-
-        filename = cmd.getOptionValue("output");
     }
 }
--- src/main/java/systems/oscillator/OscillatorSystem.java
+++ src/main/java/systems/oscillator/OscillatorSystem.java
@@ -62,4 +62,44 @@ public class OscillatorSystem {
         oscillatorSimulator = new TimeStepSimulator(TIME_DELTA, SAVE_TIME_DELTA, oscillatorCutCondition, oscillatorIntegrator, oscillatorFileGenerator, particles);
         oscillatorSimulator.simulate(true);
     }
+
+    public static void runOscillators(double timeDelta) {
+        //Solucion analitica
+        Particle oscillatorParticle = new Particle(0, new Vector(OSCILLATOR_POSITION, 0), new Vector(OSCILLATOR_VELOCITY, 0), OSCILLATOR_MASS, 0, 0, 0, 0, 0, true);
+        ForcesCalculator oscillatorForcesCalculator = new OscillatorForcesCalculator(OSCILLATOR_SPRING_CONSTANT, OSCILLATOR_VISCOSITY);
+        Integrator oscillatorIntegrator = new OscillatorAnalyticalSolutionIntegrator(oscillatorForcesCalculator, OSCILLATOR_SPRING_CONSTANT, OSCILLATOR_VISCOSITY, OSCILLATOR_AMPLITUDE);
+        FileGenerator oscillatorFileGenerator = new OscillatorFileGenerator("oscillator-analytic");
+        CutCondition oscillatorCutCondition = new TimeCutCondition(OSCILLATOR_CUTOFF_TIME);
+        TimeStepSimulator oscillatorSimulator = new TimeStepSimulator(timeDelta, SAVE_TIME_DELTA, oscillatorCutCondition, oscillatorIntegrator, oscillatorFileGenerator, Collections.singletonList(oscillatorParticle));
+        oscillatorSimulator.simulate(true);
+
+        //Integrador EULER MODIFICADO
+        oscillatorParticle = new Particle(0, new Vector(OSCILLATOR_POSITION, 0), new Vector(OSCILLATOR_VELOCITY, 0), OSCILLATOR_MASS, 0, 0, 0, 0, 0, true);
+        oscillatorForcesCalculator = new OscillatorForcesCalculator(OSCILLATOR_SPRING_CONSTANT, OSCILLATOR_VISCOSITY);
+        oscillatorIntegrator = new EulerIntegrator(oscillatorForcesCalculator);
+        oscillatorFileGenerator = new OscillatorFileGenerator("oscillator-euler");
+        oscillatorCutCondition = new TimeCutCondition(OSCILLATOR_CUTOFF_TIME);
+        oscillatorSimulator = new TimeStepSimulator(timeDelta, SAVE_TIME_DELTA, oscillatorCutCondition, oscillatorIntegrator, oscillatorFileGenerator, Collections.singletonList(oscillatorParticle));
+        oscillatorSimulator.simulate(true);
+
+        //Integrador BEEMAN
+        oscillatorParticle = new Particle(0, new Vector(OSCILLATOR_POSITION, 0), new Vector(OSCILLATOR_VELOCITY, 0), OSCILLATOR_MASS, 0, 0, 0, 0, 0, true);
+        oscillatorForcesCalculator = new OscillatorForcesCalculator(OSCILLATOR_SPRING_CONSTANT, OSCILLATOR_VISCOSITY);
+        List<Particle> particles = Collections.singletonList(oscillatorParticle);
+        oscillatorIntegrator = new BeemanIntegrator(oscillatorForcesCalculator, timeDelta, particles);
+        oscillatorFileGenerator = new OscillatorFileGenerator("oscillator-beeman");
+        oscillatorCutCondition = new TimeCutCondition(OSCILLATOR_CUTOFF_TIME);
+        oscillatorSimulator = new TimeStepSimulator(timeDelta, SAVE_TIME_DELTA, oscillatorCutCondition, oscillatorIntegrator, oscillatorFileGenerator, particles);
+        oscillatorSimulator.simulate(true);
+
+        //Integrador GEAR PREDICTOR CORRECTOR
+        oscillatorParticle = new Particle(0, new Vector(OSCILLATOR_POSITION, 0), new Vector(OSCILLATOR_VELOCITY, 0), OSCILLATOR_MASS, 0, 0, 0, 0, 0, true);
+        oscillatorForcesCalculator = new OscillatorForcesCalculator(OSCILLATOR_SPRING_CONSTANT, OSCILLATOR_VISCOSITY);
+        particles = Collections.singletonList(oscillatorParticle);
+        oscillatorIntegrator = new GearIntegrator(oscillatorForcesCalculator, particles);
+        oscillatorFileGenerator = new OscillatorFileGenerator("oscillator-gear");
+        oscillatorCutCondition = new TimeCutCondition(OSCILLATOR_CUTOFF_TIME);
+        oscillatorSimulator = new TimeStepSimulator(timeDelta, SAVE_TIME_DELTA, oscillatorCutCondition, oscillatorIntegrator, oscillatorFileGenerator, particles);
+        oscillatorSimulator.simulate(true);
+    }
 }
